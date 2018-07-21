e6330d56836c03147638c027d1ad7bc17e6577c6
Matt Farmer <mrfarmer777@gmail.com>
2018-07-21 04:24:14 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
 
     #when facebook sends back a request to /auth/facebook/callback
     #this method will find or create the user using the :uid (provider id)
-    #it will use the info hash to assign data about the user to our database
-    #it will log the user in using the session hash
-    #it will render the welcome tempalte
     @user.User.find_or_create_by(uid: auth['uid']) do |u|
+        #it will use the info hash to assign data about the user to our database
         u.name=auth['info']['name']
         u.email=auth['info']['email']
         u.image=auth['info']['image']
     end
-
+    
+    #it will log the user in using the session hash
     session[:user_id]=@user.id
 
+    #it will render the welcome tempalte
     render 'welcome/home'
   end
 

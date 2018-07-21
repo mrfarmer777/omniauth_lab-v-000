9f2bc48c6e54d2113a34406b716099333a90c589
Matt Farmer <mrfarmer777@gmail.com>
2018-07-21 04:02:19 +0000

    automatically backed up by learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
+#Using the OmniAuth::Builder object from the OmniAuth middleware
+#I think each provider would have an additional line with the requisite name, key, and secret (links to them at least)
+
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end

b75f9187e7b943871d1d3dc685f874f99187ea8b
Matt Farmer <mrfarmer777@gmail.com>
2018-07-21 04:21:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     @user.User.find_or_create_by(uid: auth['uid']) do |u|
         u.name=auth['info']['name']
         u.email=auth['info']['email']
-        u.image=auth['info']['image]
+        u.image=auth['info']['image']
       end
   end
 

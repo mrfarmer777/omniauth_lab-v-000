e8becefa0c28b38116fec5859dcb7d0bbd7a46f8
Matt Farmer <mrfarmer777@gmail.com>
2018-07-21 04:21:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
         u.name=auth['info']['name']
         u.email=auth['info']['email']
         u.image=auth['info']['image']
-      end
+    end
+
+    session[:user_id]=@user.id
   end
 
 end

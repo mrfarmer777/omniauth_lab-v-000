564cf24909eecbc44dac2f4ed6ce0312dca9ce07
Matt Farmer <mrfarmer777@gmail.com>
2018-07-21 04:23:40 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
   def create
 
+    #when facebook sends back a request to /auth/facebook/callback
+    #this method will find or create the user using the :uid (provider id)
+    #it will use the info hash to assign data about the user to our database
+    #it will log the user in using the session hash
+    #it will render the welcome tempalte
     @user.User.find_or_create_by(uid: auth['uid']) do |u|
         u.name=auth['info']['name']
         u.email=auth['info']['email']

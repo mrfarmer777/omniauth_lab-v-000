011b46566107a5882ae1ea94ba6ca31eb8c00877
Matt Farmer <mrfarmer777@gmail.com>
2018-07-21 04:26:57 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
         u.email=auth['info']['email']
         u.image=auth['info']['image']
     end
-    
+
     #it will log the user in using the session hash
     session[:user_id]=@user.id
 
